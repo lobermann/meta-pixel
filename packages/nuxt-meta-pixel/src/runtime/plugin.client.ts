834e23ef0c5e9fb88908b0e66260c4d4e60054c3
Lukas Obermann <obermann.lukas@gmail.com>
2024-09-30 09:42:01 +0200

    support for consent

--- packages/nuxt-meta-pixel/src/runtime/plugin.client.ts
+++ packages/nuxt-meta-pixel/src/runtime/plugin.client.ts
@@ -4,33 +4,46 @@ import { setup, type FacebookQuery } from 'meta-pixel'
 import { minimatch } from 'minimatch'
 import type { Plugin } from 'nuxt/app'
 
+// Flag to check if the pixel is initialized
+let pixelInitialized = false
+
 export default defineNuxtPlugin(() => {
   const runtimeConfig = useRuntimeConfig()
   const pixels = runtimeConfig.public.metapixel
   const { $fbq, init, pageView } = setup()
   $fbq.disablePushState = true
 
-  for (const name in pixels) {
-    const pixel = pixels[name]
-    init(pixel.id.toString(), pixel.autoconfig)
-  }
-
   const router = useRouter()
-  router.afterEach((to, _, failure) => {
-    if (isNavigationFailure(failure)) return
 
-    for (const name in pixels) {
-      const pixel = pixels[name]
-      const match = minimatch(to.path, pixel.pageView ?? '**')
-      if (match) {
-        pageView(pixel.id.toString())
+  // Consent function to initialize and activate the pixel
+  const consent = () => {
+    if (!pixelInitialized) {
+      for (const name in pixels) {
+        const pixel = pixels[name]
+        init(pixel.id.toString(), pixel.autoconfig)
       }
+      pixelInitialized = true
+      console.log('Pixel initialized and consent granted.')
     }
-  })
+
+    // Track the page view after consent
+    router.afterEach((to, _, failure) => {
+      if (isNavigationFailure(failure)) return
+
+      for (const name in pixels) {
+        const pixel = pixels[name]
+        const match = minimatch(to.path, pixel.pageView ?? '**')
+        if (match) {
+          pageView(pixel.id.toString())
+        }
+      }
+    })
+  }
 
   return {
     provide: {
-      fbq: $fbq
-    }
-  } 
-}) as Plugin<{fbq: FacebookQuery}>
+      fbq: $fbq,
+      consent, // Provide the consent function so it can be called elsewhere
+    },
+  }
+}) as Plugin<{ fbq: FacebookQuery, consent: () => void }>
